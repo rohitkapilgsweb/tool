4c9d4e14db11d397a5fefbdb14994ea286171e7d
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2023-08-28 09:29:00 +0530

    app changes

--- api/app.js
+++ api/app.js
@@ -25,25 +25,7 @@ app.use("/api", register);
 
 
 
-// telegram bot 
 
-const TelegramBot = require('node-telegram-bot-api');
-
-// Replace 'YOUR_BOT_TOKEN' with your actual bot token
-const bot = new TelegramBot('6353894029:AAETmmJQgft2vFgvVcuihaYozQf-7Qh3xqk', { polling: true });
-
-// Event listener for incoming messages
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const messageText = msg.text.toLowerCase(); // Convert message to lowercase
-  
-  if (messageText === 'hello') {
-    bot.sendMessage(chatId, 'Hello! How can I assist you?');
-  }
-  if (messageText.includes('hi')) {
-    bot.sendMessage(chatId, 'Hello! How can I assist you?');
-  }
-});
 
 
 // end
--- api/telegram.js
+++ api/telegram.js
@@ -1,40 +1,21 @@
-// const TelegramBot = require('node-telegram-bot-api');
+// telegram bot 
 
-// // Replace 'YOUR_BOT_TOKEN' with your actual bot token
-// const bot = new TelegramBot('6353894029:AAETmmJQgft2vFgvVcuihaYozQf-7Qh3xqk', { polling: true });
+const TelegramBot = require('node-telegram-bot-api');
 
-// // Object to store user information
-// const userInformation = {};
+// Replace 'YOUR_BOT_TOKEN' with your actual bot token
+const bot = new TelegramBot('6353894029:AAETmmJQgft2vFgvVcuihaYozQf-7Qh3xqk', { polling: true });
 
-// // Command to capture user information
-// bot.onText(/\/start/, (msg) => {
-//   const chatId = msg.chat.id;
-//   const username = msg.from.username;
+// Event listener for incoming messages
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text.toLowerCase(); // Convert message to lowercase
   
-//   userInformation[username] = chatId;
-
-//   const messageText = msg.text.toLowerCase(); // Convert message to lowercase
-  
-//   if (messageText.includes('hello')) {
-//     bot.sendMessage(chatId, 'Hello! How can I assist you?');
-//   }
-
-  
-//   bot.sendMessage(chatId, `Hello ${msg.from.first_name}! Your information is captured.`);
-// });
-
-// // Command to send a message to a user using stored chat ID
-// bot.onText(/\/send_message/, (msg) => {
-//   const chatId = msg.chat.id;
-//   const username = msg.from.username;
-
-//   if (userInformation[username]) {
-//     const targetChatId = userInformation[username];
-//     bot.sendMessage(targetChatId, 'This is a message from your bot.');
-//   } else {
-//     bot.sendMessage(chatId, 'User information not found.');
-//   }
-// });
-
-
-
+  if (messageText === 'hello') {
+    bot.sendMessage(chatId, 'Hello! How can I assist you?');
+  }else  if (messageText.includes('hi')) {
+    bot.sendMessage(chatId, 'Hello! How can I assist you?');
+  }else{
+    bot.sendMessage(chatId, 'Enter Valid Input!');
+  }
+ 
+});
