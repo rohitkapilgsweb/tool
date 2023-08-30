ed9fa2ee72701d470a6f33de006542a232f3c6b9
rohitkapilgsweb <142986343+rohitkapilgsweb@users.noreply.github.com>
2023-08-30 14:55:02 +0530

    button form

--- frontend/src/Page/automationComponents/Facebook/FB_Settings.js
+++ frontend/src/Page/automationComponents/Facebook/FB_Settings.js
@@ -1,10 +1,51 @@
 import React from 'react'
 
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/esm/Button';
+import Container from 'react-bootstrap/esm/Container';
+
 function FB_Settings() {
   return (
     <div>
-      
+      <Form>  
+       
+      <Container>
+          <Row>
+          <p className="my-4">Facebook General Settings</p>
+            <Col sm={4}>
+              <p>Enable Autoposting to Facebook</p>
+            </Col>
+            
+            <Col sm={8}>
+              <Form.Check // prettier-ignore
+                  type="switch"
+                  id="custom-switch"
+                />
+            </Col>
+          </Row> 
+        
+        
+          
+          <Row>
+          <p className="my-4">Facebook API Settings </p>
+            <Col sm={4}>
+              <p>Facebook APP Method</p>
+            </Col>
+            
+            <Col sm={8}>
+              <Button variant="primary">Add Facebook Account</Button>
+            </Col>
+            <Col sm={12} className="">
+            <Button variant="primary">Save</Button>
+            </Col>
+          </Row> 
+        </Container>
+      </Form> 
+
     </div>
+    
   )
 }
 
