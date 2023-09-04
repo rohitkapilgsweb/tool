71f36131e1c7a1db00d365334ba847ff9e778cc6
rohitkapilgsweb <142986343+rohitkapilgsweb@users.noreply.github.com>
2023-09-04 12:58:11 +0530

    create post design

--- frontend/src/Page/automationComponents/CreatePost.jsx
+++ frontend/src/Page/automationComponents/CreatePost.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Container from "react-bootstrap/Container";
+import Table from 'react-bootstrap/Table';
+import {FaRegEdit} from 'react-icons/fa';
+import {AiOutlineDelete} from 'react-icons/ai';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div>
+      <Container>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date Update</th>
+              <th>Close Date</th>   
+              <th>Check</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>yr</td>
+              <td>4-9-2023</td>
+              <td>4-9-2023</td>
+              <td><FaRegEdit/> <AiOutlineDelete/> </td>
+              <td >
+                <ul className='d-flex gap-3'>
+                  <li><span>Publish</span></li>
+                  <li> <span className=''>UnPublish</span></li>
+                </ul> 
+              </td>
+            </tr>
+            
+            
+          </tbody>
+        </Table>
+      </Container>
+    </div>  
   )
 }
 
