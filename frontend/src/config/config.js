b82187552359dd56b005d3e254b3046134b58b26
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2023-09-06 16:02:00 +0530

    add changes

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // CurrentApi: "http://localhost:8000",
-    PLACE_API_KEY:"AIzaSyBeSYHJyh5OmxQ_x4O7t_nQjDA7M9h5HmI"
+    PLACE_API_KEY:"AIzaSyBeSYHJyh5OmxQ_x4O7t_nQjDA7M9h5HmI",
     CurrentApi: "https://tool-399p.onrender.com" 
 
 }
