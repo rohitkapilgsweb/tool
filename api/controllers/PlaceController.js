38cf76ec035d0c31a4a85da74481b71de3e0241e
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2023-09-06 16:01:26 +0530

    google search implimented

--- api/controllers/PlaceController.js
+++ api/controllers/PlaceController.js
@@ -4,8 +4,12 @@ const axios = require("axios"); // Import axios
 dotenv.config();
 
 const PlaceController = expressAsyncHandler(async (req, res) => {
-    const { msg_body, nex_page } = req.body;
+    const { msg_body, next_page,back_page } = req.body;
     try {
+        if(!msg_body && !next_page){
+            res.send({status: "Search is Empty"})
+        }
+        let emptryArry= []
         if(msg_body){
             const dataPlace = await axios({
                 method: "GET",
@@ -13,19 +17,46 @@ const PlaceController = expressAsyncHandler(async (req, res) => {
                 headers: { "Accept": "application/json" },
                 params: { key: process.env.PLACE_API_KEY }, // Assuming PLACE_API_KEY is defined in your .env file
             });
-    
-            // Send the dataPlace as the response
-            res.status(200).send(dataPlace.data);
-        } else if(nex_page){
+
+            if(emptryArry === []){
+                dataPlace.data.results.map((item)=>{
+                    emptryArry.push(item.place_id)
+                })
+            }else if(emptryArry !== []){
+                emptryArry = []
+                dataPlace.data.results.map((item)=>{
+                    emptryArry.push(item.place_id)
+                })
+            }
+
+            console.log(emptryArry)
+
+            res.status(200).send({place_id: emptryArry, next_page: dataPlace.data.next_page_token});
+            // res.status(200).send({next_page: dataPlace.data.next_page_token});
+        } else if(next_page){
             const dataPlace = await axios({
                 method: "GET",
-                url: `https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken=${nex_page}`,
+                url: `https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken=${next_page}`,
                 headers: { "Accept": "application/json" },
                 params: { key: process.env.PLACE_API_KEY }, // Assuming PLACE_API_KEY is defined in your .env file
             });
     
+            if(emptryArry === []){
+                dataPlace.data.results.map((item)=>{
+                    emptryArry.push(item.place_id)
+                })
+            }else if(emptryArry !== []){
+                emptryArry = []
+                dataPlace.data.results.map((item)=>{
+                    emptryArry.push(item.place_id)
+                })
+            }
+
+            console.log(emptryArry)
+
+            res.status(200).send({place_id: emptryArry, next_page: dataPlace.data.next_page_token});
             // Send the dataPlace as the response
-            res.status(200).send(dataPlace.data);
+            // res.status(200).send(dataPlace.data);
         }
      
     } catch (error) {
--- frontend/src/Page/automationComponents/Google.jsx
+++ frontend/src/Page/automationComponents/Google.jsx
@@ -1,44 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Field } from 'react-final-form'
 import { Form } from 'react-final-form'
 import { useDispatch, useSelector } from 'react-redux'
 import { GoogleApi } from '../../redux/actions/LoginAction'
+import Loader from '../Components/Loader'
 import axios from 'axios'
 import { PLACE_API_KEY } from '../../config/config'
-
+import Pagination from 'react-bootstrap/Pagination';
+import { toast } from 'react-toastify'
+import * as XLSX from 'xlsx';
+import { IoMdCloudDownload } from 'react-icons/io'
 function Google() {
-
-  const dispatch = useDispatch()
-  const onSubmit = async values => {
-    console.log(JSON.stringify(values))
-    dispatch(GoogleApi({msg_body:JSON.stringify(values.search)}))
-    GooglePlaceData.map((item)=>{
-      getUserData(item.place_id)
-    })
-
   
-  }
-  const GooglePlaceData =  useSelector((state)=>state?.GooglePlaceSearch?.GoogleApi?.results)
-  const tokenNext =  useSelector((state)=>state?.GooglePlaceSearch?.GoogleApi?.next_page_token) 
 
-  async function getUserData(Place_Id) { 
-   
-  const url = `https://maps.googleapis.com/maps/api/place/details/json?fields=formatted_phone_number&place_id=${Place_Id}&key=${PLACE_API_KEY}`
-  try{
-    const phoneData = await  fetch(url);
-    const userData = await phoneData.json();
-    console.log(userData?.result?.formatted_phone_number,"details")
-  }
-  catch (error){
+
+const [getdata, setGetdata] = useState([]);
+const dispatch = useDispatch();
+
+const onSubmit = async (values) => {
+  console.log(JSON.stringify(values));
+  dispatch(GoogleApi({ msg_body: JSON.stringify(values?.search) }));
+};
+
+const GooglePlaceData = useSelector((state) => state?.GooglePlaceSearch?.GoogleApi?.place_id);
+const statuss = useSelector((state) => state?.GooglePlaceSearch?.GoogleApi?.status);
+const isLoading = useSelector((state) => state?.GooglePlaceSearch?.isLoading);
+const tokenNext = useSelector((state) => state?.GooglePlaceSearch?.GoogleApi?.next_page);
+console.log(isLoading)
+const fetchUserData = async (Place_Id) => {
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?fields=name,formatted_phone_number,formatted_address,url,photo&place_id=${Place_Id}&key=${PLACE_API_KEY}`;
+  try {
+    const phoneData = await fetch(url);
+    const userData = await phoneData?.json();
+    return userData?.result;
+  } catch (error) {
     console.error("Error fetching user data:", error);
+    return null;
   }
+};
+
+
+const handleDataFetching = async () => {
+  const promises = GooglePlaceData?.map((item) => fetchUserData(item));
+  const userDataArray = await Promise.all(promises);
+  
+  // Filter out null values (in case of errors)
+  const filteredUserData = userDataArray.filter((userData) => userData !== null);
+  
+  // Set the final data
+  setGetdata(filteredUserData);
+};
+
+useEffect(() => {
+  if (GooglePlaceData?.length > 0) {
+    handleDataFetching();
+  }
+
+}, [GooglePlaceData]);
+
+// Next Page
+const notify = (msg) => toast(msg);
+
+const nextOnclick = () =>{
+dispatch(GoogleApi({next_page: tokenNext}))
+if(statuss){
+  notify(statuss)
 }
 
-  console.log(GooglePlaceData)
+
+localStorage.setItem('old_page', tokenNext)
+}
+const backOnclick = () =>{
+  const Old_Token = localStorage.getItem('old_page')
+dispatch(GoogleApi({next_page: Old_Token}))
+}
+
+
+
+
+  const downloadExcel = (getdata) => {
+    const worksheet = XLSX.utils.json_to_sheet(getdata);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    XLSX.writeFile(workbook, "DataSheet_GoogleMap_place.xlsx");
+  };
+
+
   return (
     <div className='px-4'>
   
-
+{isLoading && <Loader />}
       <div className="container">
         <div className="row">
           <div className="col-md-12">
@@ -68,24 +119,32 @@ function Google() {
                 <div className="container">
                   <div className="row">
                     <div className="col-md-12">
-                
-                      {GooglePlaceData?.map((item,index)=>{
-                        return(
-                          <div key={index} className='place-card d-flex gap-3 mb-4'>
-                          <div className='place-img'>
-                            <img src={item?.icon} alt="" />
+                      {getdata && (<button className='Data_export_btn' onClick={()=>downloadExcel(getdata)}>
+   <IoMdCloudDownload size={45}/>
+</button>) }
+                    
+                  
+                        {getdata?.map((item,index)=>{
+                          return(
+                            <div key={index} className='place-card d-flex gap-3 mb-3 border py-3 px-3 rounded'>
+                            <div className='place-img'>
+                            </div>
+                            <div className='place-body'>
+                            <h5>{item?.name}</h5>
+                            <p className='m-auto'>{item?.formatted_address}</p>
+                            <p className='m-auto'>{item?.formatted_phone_number}</p>
+                            <a  href={item?.url} target="_blank" rel="noopener noreferrer">
+                          Direction
+                            </a>
+                            </div>
                           </div>
-                          <div className='place-body'>
-                          <h5>{item?.name}</h5>
-                          <p className='m-auto'>{item?.formatted_address}</p>
-                          {item?.formatted_phone_number}
-                          <a  href={`https://www.google.com/search?q=${item?.name.replace(/\s+/g, '+')}+${item?.formatted_address.replace(/\s+/g, '+')}`} target="_blank" rel="noopener noreferrer">
-                         Direction
-                          </a>
-                          </div>
-                        </div>
-                        )
-                      })}
+                          )
+                        })}
+
+<Pagination>
+      <Pagination.Prev onClick={backOnclick} />
+      <Pagination.Next onClick={nextOnclick} />
+    </Pagination>
                     </div>
                   </div>
                 </div>
