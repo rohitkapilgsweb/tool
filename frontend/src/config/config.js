68ba681b263df0c17eca51d647832fdb6beb8d44
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2023-11-07 16:18:00 +0530

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,8 +2,8 @@ module.exports = {
     // GoogleLogin:"120101465523-p8qif9nsots9gkgknlcglpdsfv3piu1a.apps.googleusercontent.com",
     //Local
     GoogleLogin:"120101465523-47t3n050jopqanbkn10t3dc3r5eeqh5b.apps.googleusercontent.com",
-    CurrentApi: "http://localhost:8000",
+    // CurrentApi: "http://localhost:8000",
     PLACE_API_KEY:"AIzaSyBqy6orfjEm0v_TDZyIj0lBc_z5WmYiyAE",
-    // CurrentApi: "https://tool-399p.onrender.com" ,
+    CurrentApi: "https://tool-399p.onrender.com" ,
 
 }
